4f1d1cac55daec8c2f83f5497b2f8268f03f9ad1
Ulugbek <u.dushanov@gmail.com>
2023-03-11 21:12:18 +0500

    feat: add modal window with context

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { CreateProduct } from './components/CreateProduct'
 import { ErrorMessage } from './components/ErrorMessage'
 import { Loader } from './components/Loader'
 import { Modal } from './components/Modal'
 import { Product } from './components/Product'
+import { ModalContext } from './context/ModalContext'
 import { IProduct } from './data/models'
 import { useProducts } from './hooks/getProducts'
 
 function App() {
 
   const {loading, error, products, addProduct} = useProducts()
-  const [modal, setModal] = useState(true)
-
+  const {modal: modalState, open: openModal, close: closeModal} = useContext(ModalContext)
   const createHandler = (product: IProduct) => {
-    setModal(false)
+    closeModal()
     addProduct(product)
   } 
 
@@ -22,9 +22,15 @@ function App() {
       {loading && <Loader />}
       {error && <ErrorMessage error={error} />}
       {products.map(product => <Product key={product.id} product={product}/>)}
-      {modal && <Modal title="Create new product">
+      {modalState && <Modal title="Create new product" onClose={() => closeModal()}>
         <CreateProduct onCreate={createHandler}/>
       </Modal>}
+      <button
+        className="fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2"
+        onClick={() => openModal()}
+      >
+        +
+      </button>
     </div>
   )
 }
